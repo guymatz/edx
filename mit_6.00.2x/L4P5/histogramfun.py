5bb11ca612696fd3a08e9fa6786e932a4eeb7f51
Guy Matz <gmatz@mdsol.com>
2014-11-01 12:46:39 -0400

    first crack

--- mit_6.00.2x/L4P5/histogramfun.py
+++ mit_6.00.2x/L4P5/histogramfun.py
@@ -24,9 +24,17 @@ def plotVowelProportionHistogram(wordList, numBins=15):
     """
     Plots a histogram of the proportion of vowels in each word in wordList
     using the specified number of bins in numBins
+    Write code to plot a histogram of the proportion of vowels in each word
+    of a given wordList. Vowels are 'A', 'E', 'I', 'O', 'U'.
     """
+    data = []
+    for w in wordList:
+        vowel_count = len([ x for x in w if x in ['a','e','i','o','u'] ])
+        prop = vowel_count/float(len(word)
+        data.append(prop)
+    pylab.hist(data, numBins)
     
 
 if __name__ == '__main__':
     wordList = loadWords()
-    plotVowelProportionHistogram(wordList)
+    plotVowelProportionHistogram(wordList, 5)
